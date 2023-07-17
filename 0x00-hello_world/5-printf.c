f89aa786d6b4f31656aac2984177b32290b159a5
abdellah-chan <chanbit.abde@gmail.com>
2023-07-17 06:31:13 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- *main - first printf
- *
- *return : return always 0 (seccess)
- */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
