23655f1d61d1ada76cc6e477ad59b1a7d937175d
abdellah-chan <chanbit.abde@gmail.com>
2023-07-19 15:35:17 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
  * main - print _putchar
  * is written by mbah
  * Return: 0
  */
 
-int main()
+int main(void)
 {
-	char string[] = "_putchar";
-	for (int i = 0; i < sizeof(string); i++)
-	{
-		putchar(string[i]);
-	}
-	putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
