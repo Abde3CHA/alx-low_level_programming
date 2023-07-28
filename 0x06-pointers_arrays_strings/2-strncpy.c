37f31160f9da5e618e1d42593fbb20b75c6493fe
abdellah-chan <chanbit.abde@gmail.com>
2023-07-28 11:48:46 -0700

    try

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-       // Copy characters from source to destination until n or null terminator
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	// If the src string was shorter than n, pad the destination with null terminators
 	for ( ; i < n; i++)
 	{
 		dest[i] = '\0';
 	}
-
 	return (dest);
 }
